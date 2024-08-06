cdbdfcecb700e30f2fc178368624d5deaf0ae560
ian-readman <ian.readman@hmcts.net>
2024-08-06 09:05:57 +0100

    PO-536-540 DB and Flyway Config plus DB Schema Scripts

---
+++ src/main/resources/db/migration/V20240730_009__insert_template_mappings.sql
@@ -0,0 +1,123 @@
+/**
+* OPAL Program
+*
+* MODULE      : insert_template_mappings.sql
+*
+* DESCRIPTION : Inserts rows of data into the TEMPLATE_MAPPINGS table. 
+*
+* VERSION HISTORY:
+*
+* Date          Author      Version     Nature of Change
+* ----------    -------     --------    ---------------------------------------------------------------------------------------------------------
+* 30/07/2024    A Dennis    1.0         PO-537 Inserts rows of data into the TEMPLATE_MAPPINGS table
+*
+**/
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000000
+  ,35
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000000
+  ,41
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000000
+  ,54
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000001
+  ,41
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000001
+  ,54
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000002
+  ,35
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000002
+  ,41
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000002
+  ,54
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000003
+  ,54
+  );
+
+INSERT INTO template_mappings
+  (
+   template_id	         
+  ,application_function_id        
+  )
+VALUES
+  (
+   500000004
+  ,500
+  );

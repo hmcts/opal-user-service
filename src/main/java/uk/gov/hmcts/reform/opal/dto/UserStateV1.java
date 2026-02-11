b4e62d0497e07d69ec2e1f61c98b37e5e6ec0f23
Russell Dodd <russell.dodd@hmcts.net>
2026-02-11 15:09:25 +0000

    PO-2810: Add new 'x-new-login' header to getUserState endpoints.

---
+++ src/main/java/uk/gov/hmcts/reform/opal/dto/UserStateV1.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.opal.dto;
+
+import java.math.BigInteger;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import uk.gov.hmcts.opal.common.dto.ToJsonString;
+import uk.gov.hmcts.opal.common.dto.Versioned;
+import uk.gov.hmcts.opal.generated.model.UserStateV1UserState;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@JsonInclude(JsonInclude.Include.NON_NULL) // This line forces the HTTP Response to be of type 'application/json'
+public class UserStateV1 extends UserStateV1UserState implements ToJsonString, Versioned {
+
+    @JsonIgnore
+    private BigInteger version;
+
+}

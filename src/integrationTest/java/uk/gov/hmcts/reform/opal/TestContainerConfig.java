035a14ab2528528560664c7086e8b6ab21f219af
Sean-Reed-CGI <Sean.Reed@HMCTS.NET>
2026-04-27 09:48:50 +0100

    PO-2835 : Cache user state (#491)
    * PO-2835 : Cache user state
    
    * PO-2835 : Cache user state - addressing PR comments
    
    * PO-2835 : Cache user state - carry if redis error (logging at warn level)
    
    * Ben's Redis config changes
    
    * Bumping chart version/ fixing aliases
    
    * Redis config fixes
    
    * Reverting env config change
    
    * DevApplication fixes and now starts Redis
    
    * Using OPAL_REDIS_CONNECTION_STRING
    
    * Using REDIS_CONNECTION_STRING
    
    * fixing redis chart name
    
    * fixing redis chart name and env variable
    
    ---------
    
    Co-authored-by: hmcts-jenkins-cnp <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/opal/TestContainerConfig.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/opal/TestContainerConfig.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.opal;
 
+import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -8,6 +9,7 @@ import org.testcontainers.utility.DockerImageName;
 public class TestContainerConfig {
 
     public static final PostgreSQLContainer POSTGRES_CONTAINER;
+    public static final RedisContainer REDIS_CONTAINER;
 
     static {
         POSTGRES_CONTAINER = new PostgreSQLContainer(DockerImageName.parse("postgres:17.5"))
@@ -16,5 +18,9 @@ public class TestContainerConfig {
             .withPassword("test");
 
         POSTGRES_CONTAINER.start();
+
+        REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:6.2.6"))
+            .withExposedPorts(6379);
+        REDIS_CONTAINER.start();
     }
 }

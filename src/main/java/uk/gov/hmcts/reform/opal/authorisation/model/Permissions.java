853f3adcababca7fa6b5c37d8b8c6196ee8d016d
Sean Reed <Sean.Reed@HMCTS.NET>
2026-05-12 11:14:36 +0100

    PO-3942 : removed unnecessary Long

--- src/main/java/uk/gov/hmcts/reform/opal/authorisation/model/Permissions.java
+++ src/main/java/uk/gov/hmcts/reform/opal/authorisation/model/Permissions.java
@@ -12,7 +12,7 @@ public enum Permissions {
     COLLECTION_ORDER(4, "Collection Order"),
     CHECK_VALIDATE_DRAFT_ACCOUNTS(5, "Check and Validate Draft Accounts"),
     SEARCH_AND_VIEW_ACCOUNTS(6, "Search and view accounts"),
-    ACCOUNT_MAINTENANCE(7L, "Account Maintenance");
+    ACCOUNT_MAINTENANCE(7, "Account Maintenance");
     public static final Permissions[] DRAFT_ACCOUNT_PERMISSIONS = {
         CREATE_MANAGE_DRAFT_ACCOUNTS, CHECK_VALIDATE_DRAFT_ACCOUNTS
     };

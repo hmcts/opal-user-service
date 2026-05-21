7853f4ba44d8a0d790dd36ee868f4582b3c209fa
shaunw-cgi <sha.williams@cgi.com>
2026-05-21 09:48:48 +0100

    PO-2879: Add content digest interceptor and filter (#542)
    * PO-2879: Add Content Digest Interceptor/Filter
    
    * Add E2E content digest response header coverage
    
    * PO-2879: Set content digest response generation in PR environment
    
    * PO-2879: Enable content digest in local Docker environment
    
    * Add default content digest headers to functional tests
    
    * PO-2879: Only send functional content digest headers when required
    
    * Add Content-Digest header to user functional tests
    
    ---------
    
    Co-authored-by: Nipa1410 <nipa.patel@hmcts.net>

--- src/functionalTest/java/uk/gov/hmcts/opal/actions/GetUserState.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/actions/GetUserState.java
@@ -7,12 +7,14 @@ import uk.gov.hmcts.opal.utils.TestHttpClient.TestHttpResponse;
 import java.util.Map;
 
 import static uk.gov.hmcts.opal.steps.BearerTokenStepDef.getToken;
+import static uk.gov.hmcts.opal.utils.ContentDigestUtils.contentDigestHeaderForEmptyBody;
 
 /**
  * Provides reusable API calls for the current-user state endpoint.
  */
 public class GetUserState {
     public static final String USER_STATE_URI = "/users/state";
+    private static final String CONTENT_DIGEST = "Content-Digest";
 
     /**
      * Retrieves the current user's state using the bearer token stored for the scenario.
@@ -20,11 +22,13 @@ public class GetUserState {
      * @param baseUri base URL of the service under test.
      */
     public static void getUserState(String baseUri) {
+        // Functional API calls include a valid digest for environments that enforce request validation.
         TestHttpResponse response = TestHttpClient.get(
             baseUri + USER_STATE_URI,
             Map.of(
                 "Content-Type", "application/json",
-                "Authorization", "Bearer " + getToken()
+                "Authorization", "Bearer " + getToken(),
+                CONTENT_DIGEST, contentDigestHeaderForEmptyBody()
             )
         );
 

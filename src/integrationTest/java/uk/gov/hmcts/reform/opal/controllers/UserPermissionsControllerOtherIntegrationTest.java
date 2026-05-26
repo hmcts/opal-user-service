5b74250842cd01f2f148535c2f1e5b49cf59cbcb
RustyHMCTS <149379845+RustyHMCTS@users.noreply.github.com>
2026-05-26 10:28:50 +0100

    Update HMCTS Dependencies - Minor/Patch (#566)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/opal/controllers/UserPermissionsControllerOtherIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/opal/controllers/UserPermissionsControllerOtherIntegrationTest.java
@@ -289,7 +289,12 @@ class UserPermissionsControllerOtherIntegrationTest extends AbstractIntegrationT
                             .header("If-Match", "0")
                             .accept(MediaType.APPLICATION_PROBLEM_JSON))
             .andExpect(status().isRequestTimeout())
-            .andExpect(content().string(""));
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.status").value(HttpStatus.REQUEST_TIMEOUT.value()))
+            .andExpect(jsonPath("$.title").value("Request Timeout"))
+            .andExpect(jsonPath("$.detail").value("Request timed out"))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/response-status"))
+            .andExpect(jsonPath("$.retriable").value(true));
     }
 
     @Test
@@ -303,7 +308,12 @@ class UserPermissionsControllerOtherIntegrationTest extends AbstractIntegrationT
                             .header("If-Match", "0")
                             .accept(MediaType.APPLICATION_PROBLEM_JSON))
             .andExpect(status().isServiceUnavailable())
-            .andExpect(content().string(""));
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.status").value(HttpStatus.SERVICE_UNAVAILABLE.value()))
+            .andExpect(jsonPath("$.title").value("Service Unavailable"))
+            .andExpect(jsonPath("$.detail").value("Service unavailable"))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/response-status"))
+            .andExpect(jsonPath("$.retriable").value(true));
     }
 
     private String createSignedToken(String subject) throws NoSuchAlgorithmException {

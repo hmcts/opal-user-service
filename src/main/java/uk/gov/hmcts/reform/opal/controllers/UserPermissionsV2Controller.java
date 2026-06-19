8c4e2402049a7f0fe09a04377032169c68e927ac
marcmollins <Marc.Mollins@HMCTS.NET>
2026-06-19 14:28:30 +0100

    DO-NOT-MERGE-FMEA-TESTING

--- src/main/java/uk/gov/hmcts/reform/opal/controllers/UserPermissionsV2Controller.java
+++ src/main/java/uk/gov/hmcts/reform/opal/controllers/UserPermissionsV2Controller.java
@@ -32,9 +32,7 @@ public class UserPermissionsV2Controller {
     @FeatureToggle(feature = RELEASE_1A, defaultValueProperty = RELEASE_1A_ENABLED_PROPERTY)
     public ResponseEntity<UserStateV2Dto> getUserStateV2(
         @PathVariable Long userId,
-        @RequestHeader(value = X_NEW_LOGIN, required = false) Boolean newLogin) throws InterruptedException {
-
-        Thread.sleep(60000L);
+        @RequestHeader(value = X_NEW_LOGIN, required = false) Boolean newLogin) {
 
         log.debug(":GET:getUserStateV2: userId: {}, new login: {}", userId, newLogin);
         if (!CURRENT_USER_ID.equals(userId)) {

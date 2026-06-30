c7374f0003254c22e12b300548655374fdc1a1fe
Sam Naylor <sam.naylor@cgi.com>
2026-06-30 19:32:44 +0100

    PO-3922 replace appMode with is-legacy-mode (#607)
    * PO-3922 replace appMode with is-legacy-mode
    
    * PO-3922 remove app-mode from application.yaml/build.gradle
    
    * PO-3922 fix checkstyle issue
    
    * Add suppression for CVE-2026-54515 related to jackson-databind
    
    ---------
    
    Co-authored-by: Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>
    Co-authored-by: cadefaulkner <cade.faulkner@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/opal/util/FeatureFlags.java
+++ src/main/java/uk/gov/hmcts/reform/opal/util/FeatureFlags.java
@@ -5,6 +5,8 @@ public final class FeatureFlags {
     public static final String DEFAULT_VALUE_PROPERTY_PREFIX = "launchdarkly.default-flag-values.";
     public static final String RELEASE_1A = "release-1a";
     public static final String RELEASE_1A_ENABLED_PROPERTY = DEFAULT_VALUE_PROPERTY_PREFIX + RELEASE_1A;
+    public static final String IS_LEGACY_MODE = "is-legacy-mode";
+    public static final String IS_LEGACY_MODE_PROPERTY = DEFAULT_VALUE_PROPERTY_PREFIX + IS_LEGACY_MODE;
 
     private FeatureFlags() {
     }
